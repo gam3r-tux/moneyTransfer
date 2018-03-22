198abf0f6cddd47f2b00c5f33921b8bfbbb85257
Jose Ramos <joserb75@gmail.com>
2018-03-22 15:27:09 -0600

    Se sube carpeta con la bd

---
+++ demoMT/dml.sql
@@ -0,0 +1,34 @@
+INSERT INTO PERSONAS (
+   NOMBRE         ,
+   APAT           ,
+   AMAT           ,
+   FCHNAC         ,
+   DIRECCION      ,
+   ESTADO         ,
+   TELEFONO       ,
+   EMAIL          ,
+   RUTAFOTO       ,
+   IDENTIFICACIONA,
+   IDENTIFICACIONB
+)
+VALUES (
+'RAMIRO','LUNA','GOMEZ', '1980-05-02', 'BLVD PEÑAFLOR #123 Col. Ensueño', 'GEORGIA', '131-212-121', 'ramlugo@gmail.com', 'C:\demoMT\avatar.jpg', 'C:\demoMT\avatar.jpg', 'C:\demoMT\avatar.jpg'
+)
+;
+INSERT INTO PERSONAS (
+   NOMBRE         ,
+   APAT           ,
+   AMAT           ,
+   FCHNAC         ,
+   DIRECCION      ,
+   ESTADO         ,
+   TELEFONO       ,
+   EMAIL          ,
+   RUTAFOTO       ,
+   IDENTIFICACIONA,
+   IDENTIFICACIONB
+)
+VALUES (
+'MARIA','LUNA','GOMEZ', '1985-10-22', 'BLVD SANTANA #303 Col. Vista Hermosa', 'WASHINGTON', '131-212-121', 'malu123@gmail.com', 'C:\demoMT\avatar.jpg', 'C:\demoMT\avatar.jpg', 'C:\demoMT\avatar.jpg'
+)
+;
